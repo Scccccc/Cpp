2ac3d271d3fb368059e45a38c2b3bc926284898d
Sccccc <yishengaaaa@gmail.com>
2023-08-23 18:36:04 +0300

    模拟实现vector：解决迭代器失效、深拷贝、拷贝构造传迭代器等问题

---
+++ Vector_7_15/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+#include "vector.h"
+
+
+
+int main()
+{
+	Mycode::test_vector8();
+	return 0;
+}
