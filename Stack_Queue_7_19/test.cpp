2ad84c135ff06da00098a76031fec84af81f5871
Sccccc <yishengaaaa@gmail.com>
2023-08-28 15:00:29 +0300

    容器适配器站和队列

---
+++ Stack_Queue_7_19/test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+#include "stack.h"
+#include "queue.h"
+
+int main()
+{
+	//Mycode::test_stack();
+	Mycode::test_queue();
+
+	return 0;
+}
